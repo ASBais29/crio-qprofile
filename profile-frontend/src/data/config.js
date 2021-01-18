4e7956bca92c5573761c0d5acd546b7750ed1ee9
Anshuman SIngh Bais <anshuman@pop-os.localdomain>
2021-01-18 23:25:33 +0530

    Data updated

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,10 @@
 
 module.exports = {
   // TODO: change your profile information here
-  name: "Ashok Kumar",
+  name: "Anshuman Singh Bais",
   greeting: "Hey 👋",
-  greetingDescription: "I'm Ashok Kumar and I'm a Software Engineer!",
-  githubUrl: "https://github.com/ashokkumar95",
-  linkedinUrl: "https://linkedin.com/ashokkumar1",
-  cvLink: "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+  greetingDescription: "I'm Anshuman Singh Bais and I'm a Software Engineer!",
+  githubUrl: "https://github.com/ASBais29",
+  linkedinUrl: "https://www.linkedin.com/in/anshuman-singh-bais-54b21a193/",
+  cvLink: "https://drive.google.com/file/d/1mh2vDnUcqF2RbPR1GZnv8YkC_yQArxLl/view?usp=sharing",
 };
